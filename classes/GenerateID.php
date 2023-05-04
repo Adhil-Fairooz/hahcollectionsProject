4de5b9130d70ee4c46c819c1f65a371d9c20855d
Adhil Fairooz <adhilzats@gmail.com>
2023-05-04 19:08:30 +0530

    Update GenerateID.php
    was modified such that to prevent repeatation of code and use Id for future items

--- classes/GenerateID.php
+++ classes/GenerateID.php
@@ -4,46 +4,17 @@
         $db = new DatabaseConnection;
         $this->conn = $db ->conn;
     }
-    public $pro_id_No;
-    public $size_id_No;
-    public $color_id_No;
-
-    public function getNewProductID(){
-        $sql = "SELECT ID_no FROM generate_id WHERE ID_Type ='product';";
+    private $id_No;
+    public function getNewID($id_Type){
+        $sql = "SELECT ID_no,Id_Prefix FROM generate_id WHERE ID_Type ='$id_Type';";
         $result = $this->conn->query($sql);
         $row =$result -> fetch_assoc();
-        $this->pro_id_No = $row['ID_no'];
-        return "P".$row['ID_no'];
+        $this->id_No = $row['ID_no'];
+        return $row['Id_Prefix'].$row['ID_no'];
     }
-    public function updateProductID(){
-        $this->pro_id_No= $this->pro_id_No+1;
-        $sql= "UPDATE generate_id SET ID_no = '$this->pro_id_No' WHERE ID_Type = 'product';";
-        $this->conn->query($sql);
-    }
-
-    public function getNewSizeID(){
-        $sql = "SELECT ID_no FROM generate_id WHERE ID_Type ='size';";
-        $result = $this->conn->query($sql);
-        $row =$result -> fetch_assoc();
-        $this->size_id_No = $row['ID_no'];
-        return "S".$row['ID_no']; 
-    }
-    public function updateSizeID(){
-        $this->size_id_No= $this->size_id_No+1;
-        $sql= "UPDATE generate_id SET ID_no = '$this->size_id_No' WHERE ID_Type = 'size';";
-        $this->conn->query($sql);
-    }
-
-    public function getNewColorID(){
-        $sql = "SELECT ID_no FROM generate_id WHERE ID_Type ='color';";
-        $result = $this->conn->query($sql);
-        $row =$result -> fetch_assoc();
-        $this->color_id_No = $row['ID_no'];
-        return "C".$row['ID_no'];
-    }
-    public function updateColorID(){
-        $this->color_id_No= $this->color_id_No+1;
-        $sql= "UPDATE generate_id SET ID_no = '$this->color_id_No' WHERE ID_Type = 'color';";
+    public function updatetID($id_Type){
+        $this->id_No= $this->id_No+1;
+        $sql= "UPDATE generate_id SET ID_no = '$this->id_No' WHERE ID_Type = '$id_Type';";
         $this->conn->query($sql);
     }
  }
