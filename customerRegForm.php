1b91fe34a1a6293fab8b05939d9318bde1c77dcf
Adhil Fairooz <adhilzats@gmail.com>
2023-07-02 23:49:40 +0530

    Create customerRegForm.php

---
+++ customerRegForm.php
@@ -0,0 +1,96 @@
+
+<?php 
+include "classes\DBConnect.php";
+include "classes\CustomerController.php";
+$db = new DatabaseConnection;
+$customerObj = new CustomerController();
+?>
+<?php
+if(isset($_POST['task']) && $_POST['task'] == 'create'){ ?>
+    <!-- modal header -->
+    <div class="modal-header my-modal-header">
+                <div class="modal-title My-regForm-title">Create Your Account</div>
+                <button type="button" class="btn-close my-close-btn" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+           <!-- header close -->
+
+           <!-- modal body -->
+           <div class="modal-body">
+                <div class="row img-row">
+                    <div class="col-md-6 img-col">
+                    <img src="assets\imgs\hah-collections-img1-assets\reg-page-image.jpg" class="img-fluid reg-img" alt="...">
+                    </div>
+                    <div class="col-md-6 d-flex justify-content-center align-items-center">
+                        <div class="card mt-2 ">
+                            <div class="card-body">
+                                <form id="customerRegisterForm">
+                                    <div class="row mb-3">
+                                        <div class="col-md-6 mb-2">
+                                        <input type="text" name="Fname" id="Fname" class="form-control myinputText" placeholder="First Name">
+                                        <div id="strFnameError"></div>
+                                        </div>
+                                        <div class="col-md-6">
+                                        <input type="text" name="Lname" id="Lname" class="form-control myinputText" placeholder="Last Name">
+                                        <div id="strLnameError"></div>
+                                        </div>
+                                    </div>
+                                    <div class="row mb-3">
+                                        <div class="col-md-12 mb-2">
+                                        <input type="text" name="email" id="email" class="form-control myinputText" placeholder="Email Address">
+                                        <div id="strEmailError"></div>
+                                        </div>
+                                    </div>
+                                    <div class="row mb-3">
+                                        <div class="col-md-12 mb-2">
+                                        <input type="password" name="password" id="password" class="form-control myinputText" placeholder="Password">
+                                        <div id="strPasswordError"></div>
+                                        </div>
+                                    </div>
+                                    <div class="row mb-3">
+                                        <div class="col-md-12 mb-2">
+                                        <input type="password" name="repeat_password" id="repeat_password" class="form-control myinputText" placeholder="Repeat Password">
+                                        <div id="strRePasswordError"></div>
+                                        </div>
+                                    </div>
+                                    <div class="row mb-3">
+                                        <div class="col"><button class="btn my-reg-btn" type="submit">Create Account</button></div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+           </div>
+           <script src="assets\js\form-validation\customer-register-form-validation.js"></script>
+           <!-- body close -->
+<?php }
+?>
+
+<?php 
+if(isset($_POST['fname']) && isset($_POST['lname']) && isset($_POST['email']) && isset($_POST['pass'])){
+    $data = [
+        "fname" => mysqli_real_escape_string($db->conn,$_POST['fname']),
+        "lname" => mysqli_real_escape_string($db->conn,$_POST['lname']),
+        "email" => mysqli_real_escape_string($db->conn,$_POST['email']),
+        "pass" => mysqli_real_escape_string($db->conn,$_POST['pass']),
+    ];
+    $result = $customerObj -> createCustomerAccount($data);
+    if($result){
+        echo 1;
+    }else{
+        echo 0;
+    }
+}
+?>
+
+<?php 
+if(isset($_REQUEST['validateEmail'])){
+    $res = $customerObj -> checkEmailInDB($_REQUEST['validateEmail']);
+    if($res){
+        echo 1;
+    }else{
+        echo 0;
+    }
+}
+
+?>
