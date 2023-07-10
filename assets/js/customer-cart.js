13912ec362f48458b2dcaf5f8fadbc177af6498e
Adhil Fairooz <adhilzats@gmail.com>
2023-07-10 19:02:43 +0530

    Update customer-cart.js

--- assets/js/customer-cart.js
+++ assets/js/customer-cart.js
@@ -109,6 +109,22 @@ function addtoCart(element){
  });
 $( document ).ready(function() {
     getCount();
+    $('#signout').on('click',function(e){
+        e.preventDefault();
+        const href="../logoutprocess.php";
+        Swal.fire({
+            title: 'Do you wish to sign out?',
+            icon: 'info',
+            showCancelButton: true,
+            confirmButtonColor: '#686de0',
+            cancelButtonColor: '#f46e50',
+            confirmButtonText: 'Yes'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                document.location.href=href;
+            }
+        });
+    });
 });
 
 
