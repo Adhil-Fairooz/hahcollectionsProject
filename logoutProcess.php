98f778131c0b70945a58c331b993ad78eaaa232a
Adhil Fairooz <adhilzats@gmail.com>
2023-07-10 19:06:40 +0530

    Update logoutProcess.php

--- logoutProcess.php
+++ logoutProcess.php
@@ -6,9 +6,15 @@ include "header.php"; ?>
     session_destroy();
 
     echo"<meta http-equiv='refresh' content='2;url=login.php'>";
-    echo"<div class='container mt-5'><div class='progress'><div class='progress-bar progress-bar-striped progress-bar-animated' role='progressbar' aria-valuenow='100' aria-valuemin='0' aria-valuemax='100' style='width: 100%''></div></div><br>
+    echo"<div class='container mt-5'>
+    <div class='progress'>
+        <div class='progress-bar progress-bar-striped progress-bar-animated' role='progressbar' aria-valuenow='100' aria-valuemin='0' aria-valuemax='100' style='width: 100%''>
             <span class='itext'>Please Wait Loging out !...</span>
-            </div>";
+        </div>
+    </div>
+    <br>
+            
+    </div>";
     ?>
 
 <?php include "footer.php";
