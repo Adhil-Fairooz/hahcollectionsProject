cff1b311aa38ba1e6e8befb11250a929e434e6fb
Adhil Fairooz <adhilzats@gmail.com>
2023-07-10 19:02:30 +0530

    Update account-creation.js

--- assets/js/account-creation.js
+++ assets/js/account-creation.js
@@ -4,8 +4,55 @@ function displayForm(){
         type: "post",
         data: {task:"create"},
         success: function(response){
-            $('.modal-content').html(response);
+            $('.regForm').html(response);
             $('#register-form-modal').modal('show');
         }
     });
 }
+
+$(document).ready(function(){
+    $("#login").submit(function(event) {
+        
+        var username = $('#username').val();
+        var password = $('#userpassword').val();
+
+        $('.form-control').removeClass('is-invalid');
+        var isUsernameValid = true;
+        var isPasswordValid = true;
+
+        $('strUserError').html('');
+        $('strpasswordError').html('');
+
+        // Validate email
+        if (username === '') {
+            $('#strUserError').html('Please enter your email.');
+            $('#username').addClass('is-invalid');
+            isUsernameValid = false;
+        } else {
+        // Regular expression for email validation
+            var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            if (!emailPattern.test(username)) {
+                $('#strUserError').html('Invalid valid email entered.');
+                $('#username').addClass('is-invalid');
+                isUsernameValid = false;
+            }
+        }
+        // password
+        if (password === '') {
+            $('#strpasswordError').html("This field is required");
+            $('#userpassword').addClass('is-invalid');
+            isPasswordValid = false;
+        }
+
+        if(isUsernameValid && isPasswordValid){
+            $('strUserError').html('');
+            $('strpasswordError').html('');
+            $('.form-control').removeClass('is-invalid');
+        }else{
+            event.preventDefault();
+        }
+
+
+      });
+});
+
