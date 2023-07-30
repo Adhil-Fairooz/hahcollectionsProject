807049b809104a5ea17c4dc70b7e4d411a2fe84e
Adhil Fairooz <adhilzats@gmail.com>
2023-07-30 18:12:30 +0530

    Update adminHeader.php

--- adminPanel/adminHeader.php
+++ adminPanel/adminHeader.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>HAH collections</title>
+    <title>HAH Admin Panel</title>
     <!-- Bootstrap-->
     <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css" />
     <!--fontawsome icons -->
