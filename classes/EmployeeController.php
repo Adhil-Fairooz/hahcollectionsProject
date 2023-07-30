db53f103d7181cb55a38ab0f844b0f8027457830
Adhil Fairooz <adhilzats@gmail.com>
2023-07-30 18:13:31 +0530

    Create EmployeeController.php

---
+++ classes/EmployeeController.php
@@ -0,0 +1,64 @@
+<?php 
+require_once "GenerateID.php";
+class EmployeeController{
+    public function __construct(){
+        $db = new DatabaseConnection;
+        $this->generateId = new GenerateID;
+        $this->conn = $db ->conn;
+    }
+    public function addEmployee($data){
+        $idType = "employee";
+        $employeeID = $this->generateId->getNewID($idType);
+        $fname = $data['fname'];
+        $lname = $data['lname'];
+        $jobRol = $data['Job_Role'];
+        $contact = $data['contact'];
+        $email = $data['email'];
+        $password = password_hash($data['pass'],PASSWORD_DEFAULT);
+        $sql_create = "INSERT INTO employee VALUES('$employeeID','$fname','$lname','$jobRol','$email','$password','$contact')";
+        if($this->conn->query($sql_create)){
+            $this->generateId->updatetID($idType);
+            return true;
+        }else{
+            return $this->conn -> error;
+        }
+    }
+
+    public function getEmpData(){
+        $sql = "SELECT * FROM employee";
+        $results = $this->conn->query($sql);
+        if($results->num_rows > 0){
+            return $results;
+        }else{
+            return false;
+        }
+    }
+
+    public function searchEMP($category,$searchData){
+        $sql = "SELECT * FROM employee WHERE $category LIKE '%$searchData%'";
+        $results = $this->conn->query($sql);
+        if($results->num_rows > 0){
+            return $results;
+        }else{
+            return false;
+        }
+    }
+    
+    public function updateEmployee($data){
+        $employeeID = $this->generateId->getNewID($idType);
+        $fname = $data['fname'];
+        $lname = $data['lname'];
+        $jobRol = $data['Job_Role'];
+        $contact = $data['contact'];
+        $email = $data['email'];
+        $password = password_hash($data['pass'],PASSWORD_DEFAULT);
+        $sql_create = "UPDATE ";
+        if($this->conn->query($sql_create)){
+            $this->generateId->updatetID($idType);
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
+?>
