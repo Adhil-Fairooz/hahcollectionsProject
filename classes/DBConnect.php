00a39db87eb4c0e6eb17a027c9400f7e3239330c
Adhil Fairooz <adhilzats@gmail.com>
2023-08-01 13:40:34 +0530

    Update DBConnect.php

--- classes/DBConnect.php
+++ classes/DBConnect.php
@@ -5,6 +5,7 @@ define('DB_Password','');
 define('DB_Database','cmos_db');
 
 class DatabaseConnection{
+    
     public function __construct(){
         $connection = new mysqli(DB_Host,DB_User,DB_Password,DB_Database);
 
