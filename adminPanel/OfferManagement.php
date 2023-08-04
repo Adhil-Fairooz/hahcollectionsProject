f728eb9284a3fa67a8b6c27217a6aa2bc5e79502
Adhil Fairooz <adhilzats@gmail.com>
2023-08-05 02:43:53 +0530

    Update OfferManagement.php

--- adminPanel/OfferManagement.php
+++ adminPanel/OfferManagement.php
@@ -16,11 +16,15 @@ $offerObj = new offerController;
     <div class="collapse navbar-collapse" id="navbarSupportedContentSub">
         <ul class="navbar-nav myNavbarNavSub justify-content-center">
             <li class="nav-item mynavitemSub">
+                <a class="nav-link mynavLinkSub <?php echo $currentSubPage == 'public' ? 'active' : '' ?>"  href="publicOffer.php">Public Offers</a>
+            </li>
+            <li class="nav-item mynavitemSub">
                 <a class="nav-link mynavLinkSub <?php echo $currentSubPage == 'private' ? 'active' : '' ?>"  href="privateOffer.php">Private Offers</a>
             </li>
             <li class="nav-item mynavitemSub">
-                <a class="nav-link mynavLinkSub <?php echo $currentSubPage == 'public' ? 'active' : '' ?>"  href="publicOffer.php">Public Offers</a>
+                <a class="nav-link mynavLinkSub <?php echo $currentSubPage == 'assignOffer' ? 'active' : '' ?>"  href="assign_private_offer.php">Assign Offer</a>
             </li>
+            
         </ul>
 
     </div>
