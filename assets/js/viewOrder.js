372ce505b3c4674bb6aa4bb596947d0839a6ff45
Adhil Fairooz <adhilzats@gmail.com>
2023-08-04 17:58:52 +0530

    Update viewOrder.js

--- assets/js/viewOrder.js
+++ assets/js/viewOrder.js
@@ -3,7 +3,61 @@ $(document).ready(function(){
     $('tbody').on('click',"#view",function(){
         var invoiceID = $(this).attr("data-invoiceID");
         var paymentID = $(this).attr("data-paymentID");
-        loadViewOrder(invoiceID,paymentID);
+        var orderstatus = $(this).attr("data-orderStatus");
+        loadViewOrder(invoiceID,paymentID,orderstatus);
+    });
+
+    $(document).on('click', '#confirmOrder', function() {
+        var invoice = $(this).attr('data-invoiceID');
+        var stat = "Confirmed";
+        Swal.fire({
+            title: 'Do you wish to Confirm this Order ?',
+            icon: 'info',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes'
+          }).then((result) => {
+              if (result.isConfirmed) {
+                updateOrderStatus(invoice,stat);
+              }
+          });
+
+        
+    });
+
+    $(document).on('click', '#readyOrder', function() {
+        var invoice = $(this).attr('data-invoiceID');
+        var stat = "Ready";
+        Swal.fire({
+            title: 'Do you wish to Confirm this Order ?',
+            icon: 'info',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes'
+          }).then((result) => {
+              if (result.isConfirmed) {
+                updateOrderStatus(invoice,stat);
+              }
+          });
+    });
+
+    $(document).on('click', '#dispatch', function() {
+        var invoice = $(this).attr('data-invoiceID');
+        var stat = "Dispatched";
+        Swal.fire({
+            title: 'Do you wish to Confirm this Order ?',
+            icon: 'info',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes'
+          }).then((result) => {
+              if (result.isConfirmed) {
+                updateOrderStatus(invoice,stat);
+              }
+          });
     });
 
     $('[name="orderStatus"]').change(function(){
@@ -58,10 +112,10 @@ function loadOrders(){
         }
     });
 }
-function loadViewOrder(invoiceID,paymentID){
+function loadViewOrder(invoiceID,paymentID,orderStatus){
     $.ajax({
         url: "orderAjaxCalls.php",
-        data: {task:"viewOrder",invoiceID:invoiceID,paymentID:paymentID},
+        data: {task:"viewOrder",invoiceID:invoiceID,paymentID:paymentID,orderStatus:orderStatus},
         success:function(response){
             $('.modal-content').html(response);
             $('#viewEachOrder').modal('show');
@@ -77,6 +131,27 @@ function loadOrderDetailsOfInvoice($invoiceID){
         success:function(response){
             $('.modal-content').html(response);
             $('#viewEachOrder').modal('show');
+        }
+    })
+}
+
+function updateOrderStatus(invoiceID,stat){
+    $.ajax({
+        url: "orderAjaxCalls.php",
+        data: {task:"updateOrderAdminStatus",invoice:invoiceID,oStat:stat},
+        success:function(response){
+            if(parseInt(response)=== 1){
+                if(stat ==='Confirmed'){Swal.fire({icon:'success',title:'Done !',text:'Order '+invoiceID+' is Confirmed'});}
+                else if(stat ==='Ready'){Swal.fire({icon:'success',title:'Done !',text:'Order '+invoiceID+' is Ready'});}
+                else if(stat ==='Dispatched'){Swal.fire({icon:'success',title:'Done !',text:'Order '+invoiceID+' is Dispatched'});}
+                else{
+                    Swal.fire({icon:'success',title:'Done !',text:'Order '+invoiceID+' Status Updated'});
+                }
+                
+            }else{
+                console.log(response);
+                Swal.fire({icon:'warning',title:'Something is not right',text:''});
+            }
         }
     })
 }
