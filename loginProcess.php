155fa2e874964a5666d345b5bace0afd31b9c14f
Adhil Fairooz <adhilzats@gmail.com>
2023-08-07 18:30:54 +0530

    Update loginProcess.php

--- loginProcess.php
+++ loginProcess.php
@@ -2,8 +2,10 @@
 include "header.php";
 include "classes\DBConnect.php";
 include "classes\CustomerController.php";
+include "classes\DeliveryDriverController.php";
 $db = new DatabaseConnection;
 $customerObj = new CustomerController();
+$driverObj = new DeliveryDriverController();
 
 if(isset($_POST['login'])){
     $user_unsafe=$_POST['username'];
@@ -41,7 +43,40 @@ if(isset($_POST['login'])){
             header("Location: login.php?status=$status");
         }
     }else{
-        /* check admin */
+        /* check delivery Driver Table */
+        $DriverResult = $driverObj->getLoginData($user);
+
+        if($DriverResult){
+            $data = $DriverResult -> fetch_assoc();
+            $driverID = $data['Driver_ID'];
+            $fname = $data['FName'];
+            $lname = $data['LName'];
+            $password = $data['Password'];
+            $verify_driver=password_verify($pass, $password);
+            if($verify_driver){
+                session_start();
+                $_SESSION['DriverID']=$driverID;
+                $_SESSION['Name']=$fname." ".$lname;
+                $_SESSION['CurrentPassword'] = $password;
+                $_SESSION['USerEmail'] = $user;
+                echo"<meta http-equiv='refresh' content='2;url=DeliveryPanel/index.php'>";
+                echo"<div class='container mt-5'>
+                        <div class='progress'>
+                            <div class='progress-bar progress-bar-striped progress-bar-animated' role='progressbar' aria-valuenow='100' aria-valuemin='0' aria-valuemax='100' style='width: 100%'>
+                            <span class='itext'>Please Wait Delivery Driver !...</span>
+                            </div>
+                        </div>
+                    <br>
+                </div>";
+            }else{
+                $status="Incorrect Password Driver !";
+                header("Location: login.php?status=$status");
+            }
+        }else{
+            /* Staff Table check */
+            $status="Incorrect Username !";
+            header("Location: login.php?status=$status");
+        }
     }
 }
 
