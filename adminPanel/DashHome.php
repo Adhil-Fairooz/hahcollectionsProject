340d7c690c0755e178f50af47895d59dfc22c7b8
Adhil Fairooz <adhilzats@gmail.com>
2023-08-07 18:28:48 +0530

    Update DashHome.php

--- adminPanel/DashHome.php
+++ adminPanel/DashHome.php
@@ -2,53 +2,94 @@
 $currentSubPage="adminHome";
 include "admin.php"; ?>
 <link rel="stylesheet" href="..\assets\css\admin-dashboard-home-style-A.css">
+
 <div class="card main-container">
+
     <div class="card-body main-body">
+
         <div class="row main-row">
+
             <div class="col main-col">
+
                 <div class="card sub-card">
+
                     <div class="card-header my-card-header">Category Status</div>
+
                     <div class="card-body">
+
                         <div class="row">
+
                             <div class="col">
+
                                 <div class="card my-card-content-box">
+
                                     <div class="card-body my-content-box-body">
+
                                         <div class="card-title my-content-box-title">Main</div>
+
                                         <div class="card-text my-content-box-text">4</div>
                                         
                                     </div>
+
                                 </div>
+
                             </div>
+
                             <div class="col">
+
                                 <div class="card my-card-content-box">
+
                                     <div class="card-body my-content-box-body">
+
                                         <div class="card-title my-content-box-title">Sub</div>
+
                                         <div class="card-text my-content-box-text">16</div>
+
                                     </div>
+
                                 </div>
+
                             </div>
+
                             <div class="col">
+
                                 <div class="card my-card-content-box">
+
                                     <div class="card-body my-content-box-body">
+
                                         <div class="card-title my-content-box-title">Brand</div>
+
                                         <div class="card-text my-content-box-text">5</div>
                                         
                                     </div>
+
                                 </div>
+
                             </div>
+
                             <div class="col">
+
                                 <div class="card my-card-content-box">
+
                                     <div class="card-body my-content-box-body">
+
                                         <div class="card-title my-content-box-title">Supplier</div>
+
                                         <div class="card-text my-content-box-text">5</div>
                                         
                                     </div>
+
                                 </div>
+
                             </div>
+
                         </div>
+
                     </div>
+
                 </div>
             </div>
+            
             <div class="col main-col">
                 <div class="card sub-card">
                     <div class="card-header my-card-header">Order Status</div>
