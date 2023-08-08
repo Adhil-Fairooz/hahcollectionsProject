b663350e3a6806176785b9a41564965bbf3ac36c
Adhil Fairooz <adhilzats@gmail.com>
2023-08-09 02:30:31 +0530

    Update invoiceAjax.php

--- customerPanel/invoiceAjax.php
+++ customerPanel/invoiceAjax.php
@@ -39,7 +39,7 @@ if(isset($_REQUEST['task']) && $_REQUEST['task']==='displayInvoice'){
 
                     <div class="row mt-2">
 
-                        <div class="col"><button class="btn btn-outline-primary" data-invoiceID = '<?=$row['Invoice_ID']?>' data-paymentID='<?=$row['Payment_ID']?>' id="cust-view-order">View Order</button></div>
+                        <div class="col"><button class="btn btn-outline-primary" data-orderStatus="<?=$row['order_status']?>" data-invoiceID = '<?=$row['Invoice_ID']?>' data-paymentID='<?=$row['Payment_ID']?>' id="cust-view-order">View Order</button></div>
                         
                         <?php if($row['order_status'] ==='Pending') {?>
 
@@ -116,7 +116,7 @@ if(isset($_REQUEST['task']) && $_REQUEST['task']==='displayCompletedInvoice'){
 
                     <div class="row mt-2">
 
-                        <div class="col"><button class="btn btn-outline-primary" data-invoiceID = '<?=$row['Invoice_ID']?>' data-paymentID='<?=$row['Payment_ID']?>' id="cust-view-order">View Order</button></div>
+                        <div class="col"><button class="btn btn-outline-primary rating-viewbtn" data-invoiceID = '<?=$row['Invoice_ID']?>' data-paymentID='<?=$row['Payment_ID']?>' data-orderStatus="<?=$row['order_status']?>" id="">View Order</button></div>
                         
                         <?php if($row['order_status'] ==='Pending') {?>
 
@@ -214,6 +214,156 @@ if(isset($_REQUEST['task'])&& isset($_REQUEST['invoice']) && $_REQUEST['task']==
                                         <td><?=$row['Size_ID']?>: <?=$row['Size_Value']?></td>
                                         <td><?=$row['Color_ID']?>: <i class="fas fa-square" style="color: <?=$row['Color_Value']?>"></i>: <?=$row['Color_Name']?></td>
                                         <td><?=$row['Order_Qty']?></td>
+                                    </tr>
+                                <?php
+                            }
+                            ?>
+                        </tbody> 
+
+                    </table>
+
+                </div>
+
+            </div>
+
+            <div class="col-md-4">
+
+                <div class="card">
+
+                    <div class="card-header mysubcardheader">Your InFo Details</div>
+
+                    <div class="card-body">
+
+                        <div class="row">
+
+                            <div class="col-md-6 label">Delevery Address:</div>
+
+                            <div class="col"><?=$address?></div>
+
+                        </div>
+
+                        <div class="row mt-3">
+                            <div class="col-md-6 label">Contact No:</div>
+                            <div class="col"><?=$contact?></div>
+                        </div>
+                        <div class="row mt-3">
+
+                            <div class="col-md-6 label">Payment Method:</div>
+
+                            <div class="col">
+
+                            <?php 
+                                if($pmValue === 'COD'){
+                            ?>
+                                Cash on Delivery
+                            <?php
+                            }else if($pmValue === 'BD'){
+                            ?>
+                                Bank Deposit
+                            <?php
+                            }else{
+                            ?>
+                                Pick Up
+                            <?php
+                            }
+                            ?>
+                            </div>
+
+                        </div>
+                            <?php if($pmValue === 'BD'){?>
+
+                            <div class="row mt-3">
+
+                                <div class="col-md-6 label">Payment Proof:</div>
+
+                                    <div class="col">
+
+                                        <?php 
+                                            $payBDResult=$orderObj->getImgProofBDPayment($_REQUEST['payment']);
+                                            if($payBDResult){
+                                                $pBDData = $payBDResult->fetch_assoc();
+                                                $payProof = $pBDData['Payment_Proof'];
+                                                if(is_null($payProof)){
+                                                    ?><a href="#" class="btn btn-outline-danger">No Image</a><?php
+                                                }else{
+                                                    ?><a href="data:image/jpg;base64,<?=base64_encode($payProof)?>"  target="_blank" class="btn btn-outline-success" id="proofimgview">view Image</a><?php
+                                                }
+                                            }
+                                        ?>
+                                            
+                                    </div>
+                                </div>
+                                    <?php
+                                } ?>
+                                
+                            </div>
+                    </div>
+
+                </div>
+
+        </div>
+
+    </div>
+
+    <?php
+}
+
+if(isset($_REQUEST['task'])&& isset($_REQUEST['invoice']) && $_REQUEST['task']==='viewCustomerOrderWithRating'){
+    $pmResult = $orderObj->getPaymentMethod($_REQUEST['payment']);
+    $pmData = $pmResult->fetch_assoc();
+    $pmValue = $pmData['Payment_METHOD'];
+    $contact;$address;
+    if($pmValue === "Pick"){
+        $contact = $pmData['Contact_NO'];
+        $address = "None";
+    }else{
+        $contact=$pmData['Contact_NO'];
+        $address=$pmData['Delivery_Address'];
+    }
+    ?>
+    <!--Modal Header -->
+    <div class="modal-header my-modal-header mycardheader">
+
+        <div class="modal-title My-modal-title">INVOICE #<?=$_REQUEST['invoice']?> Order </div>
+
+        <button type="button" class="btn-close my-close-btn" data-bs-dismiss="modal" aria-label="Close"></button>
+
+    </div>
+    <!-- ------------- -->
+    <!-- Modal body -->
+    <div class="modal-body">
+
+        <div class="row">
+
+            <div class="col-md-8">
+
+                <div class="table-responsive">
+
+                    <table class="table table-sm">
+
+                        <thead>
+
+                            <tr>
+                                <th scope="col">Product</th>
+                                <th scope="col">Size</th>
+                                <th scope="col">Color</th>
+                                <th scope="col">Quantity</th>
+                                <th scope="col">Action</th>
+                            </tr>
+
+                        </thead>
+
+                        <tbody id="modalOrder">
+                            <?php 
+                            $result = $orderObj->getOrderInFoTable($_REQUEST['invoice']);
+                            foreach($result as $row){
+                                ?>
+                                    <tr>
+                                        <td><?=$row['Product_ID']?>: <?=$row['Pro_Name']?></td>
+                                        <td><?=$row['Size_ID']?>: <?=$row['Size_Value']?></td>
+                                        <td><?=$row['Color_ID']?>: <i class="fas fa-square" style="color: <?=$row['Color_Value']?>"></i>: <?=$row['Color_Name']?></td>
+                                        <td><?=$row['Order_Qty']?></td>
+                                        <td><button class="btn btn-outline-success" id="RateButton" data-productID='<?=$row['Product_ID']?>'>Rate</button></td>
                                     </tr>
                                 <?php
                             }
