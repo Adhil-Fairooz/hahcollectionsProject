aa4ad56ba5a2957b1ffef81207054ba43b0f3907
Adhil Fairooz <adhilzats@gmail.com>
2023-08-10 16:21:18 +0530

    Update SupplierController.php

--- classes/SupplierController.php
+++ classes/SupplierController.php
@@ -30,5 +30,34 @@ class SupplierController{
             return false;
         }
     }
+    public function getSuplierDataForUpdate($id){
+        $sql_get_supplier_data = "SELECT * FROM supplier WHERE Sup_ID='$id';";
+        $results = $this->conn->query($sql_get_supplier_data);
+        if($results->num_rows > 0){
+            return $results;
+        }else{
+            return false;
+        }
+    }
+    public function UpdateSupplier($data){
+        $id = $data['id'];
+        $name = $data['name'];
+        $email = $data['email'];
+        $contact = $data['contact'];
+        $sql = "UPDATE supplier SET Sup_Name = '$name',Sup_contact ='$contact',Email = '$email' WHERE Sup_ID='$id'";
+        if($this->conn->query($sql)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public function DeleteSupplierFromDB($id){
+        $sql = "DELETE FROM supplier WHERE Sup_ID='$id'";
+        if($this->conn->query($sql)){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
 ?> 
