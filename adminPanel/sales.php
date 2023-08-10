9096d7e5ff391b2ac40f8ed11eb3974bc21e6000
Adhil Fairooz <adhilzats@gmail.com>
2023-08-10 09:41:01 +0530

    Update sales.php

--- adminPanel/sales.php
+++ adminPanel/sales.php
@@ -4,37 +4,9 @@ include "admin.php"; ?>
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 <div class="card mt-5">
     <div class="card-body">
-    <canvas id="myChart" style="width:100%;max-width:100%;max-height: 500px"></canvas>
-    <?php 
-    echo "
-    <script>
-    const ctx = document.getElementById('myChart');
-
-    new Chart(ctx, {
-      type: 'bar',
-      data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange','Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3,12, 19, 3, 5, 2, 3],
-          borderWidth: 1
-        }]
-      },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true
-          }
-        }
-      }
-    });
-    </script>
-    
-    ";
-    
-    ?>
+    <canvas id="myProductSalesChart" style="width:100%;max-width:100%;max-height: 500px"></canvas>
     
     </div>
 </div>
-
+<script src="..\assets\js\adminChartGeneration.js"></script>
 <?php include "adminFooter.php"; ?>
