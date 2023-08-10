6257541b8fd7024bf5a117a236e2659c0512a2e6
Adhil Fairooz <adhilzats@gmail.com>
2023-08-11 00:59:50 +0530

    Update logoutProcess.php

--- logoutProcess.php
+++ logoutProcess.php
@@ -11,6 +11,9 @@ include "header.php"; ?>
     if(isset($_SESSION['Driver_ID'])){
         unset($_SESSION['Driver_ID']);
     }
+    if(isset($_SESSION['Admin_ID'])){
+        unset($_SESSION['Admin_ID']);
+    }
     session_destroy();
 
     echo"<meta http-equiv='refresh' content='2;url=login.php'>";
